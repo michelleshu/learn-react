6ff723a059ee04cda8d8f42dfa17bf841542bc4e
Michelle Shu <shu.michelle.w@gmail.com>
2018-02-19 18:27:46 -0500

    React Router and Hello World

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+
 import App from './App';
+import HelloWorld from './components/HelloWorld';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Route exact path="/" component={App} />
+                <Route exact path= "/hello-world" component={HelloWorld} />
+            </div>
+        </BrowserRouter>
+    );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
